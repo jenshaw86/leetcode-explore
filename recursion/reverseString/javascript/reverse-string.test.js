e2e1322d86eeaffab2c91b3a3317f9a686110cc2
Jenny Shaw <bloo.j.shaw@gmail.com>
2021-05-22 17:05:52 -0700

    Add reverse-string notes, solution, and tests

---
+++ recursion/reverseString/javascript/reverse-string.test.js
@@ -0,0 +1,19 @@
+const {reverseString, reverse} = require('./reverse-string');
+
+test('returns ["c", "b", "a"]', () => {
+    const s = ["a","b","c"];
+    const reversedS = ["c", "b", "a"];
+    expect(reverseString(s)).toEqual(reversedS);
+});
+
+test('returns ["o","l","l","e","H"]', () => {
+    const s = ["H","e","l","l","o"];
+    const reversedS = ["o","l","l","e","H"];
+    expect(reverseString(s)).toEqual(reversedS);
+});
+
+test('returns ["h","a","n","n","a","H"]', () => {
+    const s = ["H","a","n","n","a","h"];
+    const reversedS = ["h","a","n","n","a","H"];
+    expect(reverseString(s)).toEqual(reversedS);
+});
