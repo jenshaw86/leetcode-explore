a216e3b09b572c5769eba7b98de8c28baa27f1c0
Jenny Shaw <bloo.j.shaw@gmail.com>
2021-05-24 14:20:29 -0700

    Add recursive solution for reverse-list

--- recursion/reverse-linked-list/javascript/reverse-linked-list.js
+++ recursion/reverse-linked-list/javascript/reverse-linked-list.js
@@ -6,37 +6,47 @@ Edge Cases: list can be empty or hold a single node
 */
 
 /* Recursive solution 
-
+Time: 0(n)
+Space: 0(n)
 */
-// const reverseList = head => {
+const reverseList = (head, prev=null) => {
+// base case: when there is no next node, in other words, curr is the last valid node in the list
+// recurrence relation: prev, curr, next, each shifted to the next node
+    if (!head) return head;
     
-// }
+    let next = head.next;
+    head.next = prev;
+    
+    // if there's no next node, return the current head.
+    // otherwise, call reverse the remainder of the list
+    return next ? reverseList(next, head) : head;
+}
 
 /* Iterative solution
 Time: O(n)
 Space: O(1)
 */
 
-const reverseList = head => {
-    if (!head || !head.next) return head;
+// const reverseList = head => {
+//     if (!head || !head.next) return head;
 
-    let prev = head; 
-    let curr = prev.next;
-    let next = curr.next;
+//     let prev = head; 
+//     let curr = prev.next;
+//     let next = curr.next;
 
-    prev.next = null;
-    curr.next = prev;
+//     prev.next = null;
+//     curr.next = prev;
 
-    while(next) {    
-        prev = curr;
-        curr = next;
-        next = next.next;
+//     while(next) {    
+//         prev = curr;
+//         curr = next;
+//         next = next.next;
 
-        curr.next = prev;
-    }
+//         curr.next = prev;
+//     }
 
-    return curr;
+//     return curr;
 
-}
+// }
 
 module.exports = reverseList;
