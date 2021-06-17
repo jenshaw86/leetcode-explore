6e99b6ed0cf3f08f9b6cb6e5665e7ef43a785fb8
Jenny Shaw <bloo.j.shaw@gmail.com>
2021-06-17 15:28:32 -0700

    Add remove-duplicates solution

--- arrays-101/remove-duplicates/javascript/remove-duplicates.js
+++ arrays-101/remove-duplicates/javascript/remove-duplicates.js
@@ -7,7 +7,7 @@ Edge:
 
 Solution:
 Use two pointer.
-Start one pointer (j) at idx 0, and second pointer(i) at i + 0
+Start one pointer (j) at idx 0, and second pointer(i) at j + 0
 j represents the length of the array after duplicates are removed, 
 i is a comparison point
 Compare the two values,
@@ -15,3 +15,18 @@ Compare the two values,
     Once i reaches a new value, increment j, and replace the value at index j with the value at index i, then increment i    
 */
 
+const removeDuplicates = nums => {
+    if (nums.length <= 1) return nums.length;
+
+    let j = 0;
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[j]) {
+            j++;
+            nums[j] = nums[i];
+        }
+    }
+
+    return j + 1;
+}
+
+module.exports = removeDuplicates;
